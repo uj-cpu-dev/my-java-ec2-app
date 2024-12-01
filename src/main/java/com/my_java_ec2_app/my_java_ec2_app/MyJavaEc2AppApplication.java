64a9566b712f10a6c76a4ac351e7aad7b2918d04
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2024-12-01 21:26:50 +0000

    first commit

---
+++ src/main/java/com/my_java_ec2_app/my_java_ec2_app/MyJavaEc2AppApplication.java
@@ -0,0 +1,13 @@
+package com.my_java_ec2_app.my_java_ec2_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaEc2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJavaEc2AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my_java_ec2_app/my_java_ec2_app/MyJavaEc2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_java_ec2_app.my_java_ec2_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaEc2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
