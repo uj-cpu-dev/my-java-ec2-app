961738a701b253e9ef0b3c4ce23dbaf67da03893
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-01-29 05:45:26 +0000

    testing v2

--- jenkins/scripts/build-and-push.sh
+++ jenkins/scripts/build-and-push.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 set -e
-
-# Get the system architecture (e.g., amd64, arm64)
-ARCHITECTURE=$(uname -m)
-
-# Set platform based on the architecture
-if [[ "$ARCHITECTURE" == "x86_64" ]]; then
-    PLATFORM="linux/amd64"
-elif [[ "$ARCHITECTURE" == "aarch64" ]]; then
-    PLATFORM="linux/arm64"
-else
-    echo "Unsupported architecture: $ARCHITECTURE"
-    exit 1
-fi
-
-echo "Building Docker image for platform $PLATFORM..."
+echo "Building Docker image..."
 IMAGE_TAG=$(cat image-tag.txt)
-
-# Build Docker image for the selected platform
-docker build --platform $PLATFORM -t $REPO_NAME:$IMAGE_TAG .
-
-# Tag the image for pushing to ECR
+docker build --platform linux/arm64 -t $REPO_NAME:$IMAGE_TAG .
 docker tag $REPO_NAME:$IMAGE_TAG $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG
-
 echo "Pushing Docker image to ECR..."
 docker push $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG
