e9700590668a34058df1eecb731e2d09ba8b2f03
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-01-29 05:04:36 +0000

    v9

--- jenkins/scripts/build-and-push.sh
+++ jenkins/scripts/build-and-push.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Building Docker image..."
 IMAGE_TAG=$(cat image-tag.txt)
-docker build --platform linux/amd64 -t $REPO_NAME:$IMAGE_TAG .
+docker build --platform linux/amd64, linux/arm64 -t $REPO_NAME:$IMAGE_TAG .
 docker tag $REPO_NAME:$IMAGE_TAG $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG
 echo "Pushing Docker image to ECR..."
 docker push $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG
