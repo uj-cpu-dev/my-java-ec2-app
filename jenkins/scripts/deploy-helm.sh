fdf1575d79447d66ce5cfe872aaa4ddab738afea
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-01-29 01:30:02 +0000

    helm v4

--- jenkins/scripts/deploy-helm.sh
+++ jenkins/scripts/deploy-helm.sh
@@ -6,4 +6,5 @@ helm upgrade --install my-app ./my-app \
     --namespace $NAMESPACE \
     --set image.repository=$ECR_REGISTRY/$REPO_NAME \
     --set image.tag=$IMAGE_TAG \
-    --set image.pullPolicy=IfNotPresent
+    --set image.pullPolicy=IfNotPresent \
+    --kubeconfig ~/.kube/config
