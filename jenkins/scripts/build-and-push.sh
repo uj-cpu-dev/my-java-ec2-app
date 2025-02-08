0d07d50fcc2bd3cbef9413da9cd208b9c38a3795
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-02-08 02:18:52 +0000

    test v5

--- jenkins/scripts/build-and-push.sh
+++ jenkins/scripts/build-and-push.sh
@@ -1,47 +1,6 @@
 #!/bin/bash
 set -e
 
-# Function to install Trivy
-install_trivy() {
-    echo "Installing Trivy..."
-
-    # Step 1: Get the latest Trivy version
-    TRIVY_VERSION=$(curl -s https://api.github.com/repos/aquasecurity/trivy/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-    echo "Latest Trivy version: $TRIVY_VERSION"
-
-    # Step 2: Download the Trivy binary
-    echo "Downloading Trivy binary..."
-    curl -LO https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz
-
-    # Step 3: Extract the binary
-    echo "Extracting Trivy binary..."
-    tar -xzf trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz
-
-    # Step 4: Move the binary to /usr/local/bin
-    echo "Moving Trivy binary to /usr/local/bin..."
-    sudo mv trivy /usr/local/bin/
-
-    # Step 5: Clean up
-    echo "Cleaning up..."
-    rm trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz
-
-    # Step 6: Verify the installation
-    echo "Verifying Trivy installation..."
-    trivy --version
-
-    if [ $? -eq 0 ]; then
-        echo "Trivy installed successfully."
-    else
-        echo "Failed to install Trivy."
-        exit 1
-    fi
-}
-
-# Check if Trivy is installed, else install it
-if ! command -v trivy &> /dev/null; then
-    install_trivy
-fi
-
 echo "Building Docker image..."
 IMAGE_TAG=$(cat image-tag.txt)
 
@@ -58,17 +17,18 @@ fi
 # Build multi-platform image
 docker buildx build --platform linux/amd64,linux/arm64 -t $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG --push .
 
-# Scan image for vulnerabilities
-echo "Scanning Docker image for vulnerabilities..."
-export TRIVY_CACHE_DIR=/var/lib/jenkins/trivy-cache
+# Define Trivy cache directory (optional, speeds up future scans)
+export TRIVY_CACHE_DIR=/tmp/trivy-cache
 mkdir -p $TRIVY_CACHE_DIR
 
-# Update DB first
-trivy --cache-dir $TRIVY_CACHE_DIR image --download-db-only
+# Run Trivy in a Docker container
+echo "Scanning Docker image for vulnerabilities..."
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $TRIVY_CACHE_DIR:/root/.cache/trivy \
+    aquasec/trivy:latest image --scanners vuln --timeout 5m $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG
 
-# Scan the image without updating the DB
-trivy image --scanners vuln --skip-db-update --cache-dir $TRIVY_CACHE_DIR --no-progress --timeout 5m $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG
-
+# Check the exit status of Trivy
 if [ $? -eq 1 ]; then
     echo "Image scanning failed due to HIGH or CRITICAL vulnerabilities."
     exit 1
