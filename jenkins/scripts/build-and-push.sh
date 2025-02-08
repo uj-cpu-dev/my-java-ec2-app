1a201d3c149bdac5e2629e92f3f2915cf6e48de7
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2025-02-08 01:26:10 +0000

    test v2

--- jenkins/scripts/build-and-push.sh
+++ jenkins/scripts/build-and-push.sh
@@ -60,7 +60,9 @@ docker buildx build --platform linux/amd64,linux/arm64 -t $ECR_REGISTRY/$REPO_NA
 
 # Scan image for vulnerabilities
 echo "Scanning Docker image for vulnerabilities..."
-trivy image --scanners vuln --cache-dir /var/lib/trivy --timeout 5m $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG
+export TRIVY_CACHE_DIR=/tmp/trivy
+mkdir -p $TRIVY_CACHE_DIR
+trivy image --scanners vuln --cache-dir $TRIVY_CACHE_DIR --timeout 5m $ECR_REGISTRY/$REPO_NAME:$IMAGE_TAG
 
 if [ $? -eq 1 ]; then
     echo "Image scanning failed due to HIGH or CRITICAL vulnerabilities."
